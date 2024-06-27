ca4b4845b7c159a39f5c68ad8f7f76cb6f4d6963
Brandon Williams <brandon@mystenlabs.com>
2024-06-27 12:27:27 -0500

    schema: skip serializing example if None

--- src/v3_1/schema.rs
+++ src/v3_1/schema.rs
@@ -17,13 +17,13 @@ pub struct SchemaObject {
     /// been deprecated in favor of the JSON Schema `examples` keyword. Use
     /// of `example` is discouraged, and later versions of this
     /// specification may remove it.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub example: Option<serde_json::Value>,
 }
 
 #[cfg(feature = "conversions")]
 mod conversions {
     use crate::v3_0;
-    use serde_json::Value as JValue;
     impl From<v3_0::Schema> for super::SchemaObject {
         fn from(s: v3_0::Schema) -> Self {
             let oldval = serde_json::to_value(&s).expect("Convert Schema to serde_json::Value");
